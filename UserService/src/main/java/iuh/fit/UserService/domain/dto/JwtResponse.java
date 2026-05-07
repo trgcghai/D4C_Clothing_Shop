f47730258b642a3140244e67d4e71fdaf824818a
trgcghai <conghai.tpma@gmail.com>
2026-05-07 13:29:09 +0700

    fix: remove duplicate constructor from JwtResponse (Lombok @AllArgsConstructor handles it)

--- UserService/src/main/java/iuh/fit/UserService/domain/dto/JwtResponse.java
+++ UserService/src/main/java/iuh/fit/UserService/domain/dto/JwtResponse.java
@@ -16,17 +16,4 @@ public class JwtResponse {
     private String avatar;
     private String role;
     private String refreshToken;
-
-    public JwtResponse(String token, String type, Long id, String username, String email, String fullName, String phoneNumber, String avatar, String role, String refreshToken) {
-        this.token = token;
-        this.type = type;
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.fullName = fullName;
-        this.phoneNumber = phoneNumber;
-        this.avatar = avatar;
-        this.role = role;
-        this.refreshToken = refreshToken;
-    }
 }
