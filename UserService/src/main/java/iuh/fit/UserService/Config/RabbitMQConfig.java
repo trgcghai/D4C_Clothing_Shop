e6af2d44b78ba8a29f8c8f6c631cf3b0d4e5fd6d
trgcghai <conghai.tpma@gmail.com>
2026-05-09 00:20:35 +0700

    feat(userservice): configure RabbitTemplate with publisher confirms

---
+++ UserService/src/main/java/iuh/fit/UserService/Config/RabbitMQConfig.java
@@ -0,0 +1,25 @@
+package iuh.fit.UserService.Config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(RabbitMQConfig.class);
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setConfirmCallback((correlationData, ack, cause) -> {
+            if (!ack) {
+                log.error("Message not confirmed. Cause: {}", cause);
+            }
+        });
+        return template;
+    }
+}
