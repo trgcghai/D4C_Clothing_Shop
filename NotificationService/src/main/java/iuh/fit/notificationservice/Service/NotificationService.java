279eee5c871ae5b5b524f5415de3d7a9ec2b584e
trgcghai <conghai.tpma@gmail.com>
2026-05-12 08:13:40 +0700

    NotificationService: create AccountEvent DTO and add sendAccountUnlockedEmail interface method

--- NotificationService/src/main/java/iuh/fit/notificationservice/Service/NotificationService.java
+++ NotificationService/src/main/java/iuh/fit/notificationservice/Service/NotificationService.java
@@ -1,6 +1,6 @@
 package iuh.fit.notificationservice.Service;
 
-import iuh.fit.notificationservice.Domain.DTO.AccountLockedEvent;
+import iuh.fit.notificationservice.Domain.DTO.AccountEvent;
 import iuh.fit.notificationservice.Domain.DTO.NotificationResponse;
 import iuh.fit.notificationservice.Domain.DTO.SendNotificationRequest;
 import iuh.fit.notificationservice.Domain.DTO.SendVerificationEmailRequest;
@@ -14,5 +14,7 @@ public interface NotificationService {
 
     void sendVerificationEmail(VerificationEmailEvent event);
 
-    void sendAccountLockedEmail(AccountLockedEvent event);
+    void sendAccountLockedEmail(AccountEvent event);
+
+    void sendAccountUnlockedEmail(AccountEvent event);
 }
