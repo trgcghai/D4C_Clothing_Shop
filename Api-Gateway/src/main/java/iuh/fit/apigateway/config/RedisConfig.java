e5d7ae47a6e7065e0f50511f6a7ba89c614e28b8
trgcghai <conghai.tpma@gmail.com>
2026-05-28 08:50:54 +0700

    fix: add @Primary to resolve ReactiveRedisTemplate bean conflict

--- Api-Gateway/src/main/java/iuh/fit/apigateway/config/RedisConfig.java
+++ Api-Gateway/src/main/java/iuh/fit/apigateway/config/RedisConfig.java
@@ -2,6 +2,7 @@ package iuh.fit.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -10,6 +11,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Primary
     @Bean
     public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(
             ReactiveRedisConnectionFactory connectionFactory) {
