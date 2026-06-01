35804b045968772580364a1a831c3db849744b0d
trgcghai <conghai.tpma@gmail.com>
2026-06-01 22:21:10 +0700

    feat: implement DatabaseMigrationRunner for OrderService and PaymentService with shedlock table creation

--- ProductService/src/config/redis.config.js
+++ ProductService/src/config/redis.config.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379"
+  url: "redis://redis:6379",
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
