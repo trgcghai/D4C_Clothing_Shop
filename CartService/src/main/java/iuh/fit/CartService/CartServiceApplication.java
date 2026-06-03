88497af387550ad0cc02edc6f0d8de8c1a2a0320
Hung <22645171.hung@student.iuh.edu.vn>
2026-06-03 16:06:11 +0700

    feat(CartService): add @EnableRabbit to enable @RabbitListener support

--- CartService/src/main/java/iuh/fit/CartService/CartServiceApplication.java
+++ CartService/src/main/java/iuh/fit/CartService/CartServiceApplication.java
@@ -3,9 +3,11 @@ package iuh.fit.CartService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRabbit
 public class CartServiceApplication {
 
 	public static void main(String[] args) {
